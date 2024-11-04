c5ee9713c5fb89c76a383df719bedb6aaf8dcc64
andreacagnina <amirkal@libero.it>
2024-11-04 16:34:22 +0100

    [scaffolding]

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script>
+import HellloWorld from './components/HellloWorld.vue'
+export default {
+  components: {
+    HellloWorld,
+  }
+
+}
+</script>
+
+<template>
+  <HellloWorld />
+</template>
+
+<style lang="scss">
+@use './styles/generals.scss';
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,11 @@
+<script>
+export default {
+
+}
+</script>
+
+<template>
+  <h1>ciao bei maschioni</h1>
+</template>
+
+<style lang="scss" scoped></style>
