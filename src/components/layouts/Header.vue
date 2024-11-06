04380f4440b12a22ceb1fa9fe296f0e1c4df4e83
LorenzoPranio <lorenzopranio02@gmail.com>
2024-11-06 18:24:08 +0100

    [UPD]Added header

--- src/components/layouts/Header.vue
+++ src/components/layouts/Header.vue
@@ -8,17 +8,27 @@ name: 'AppHeader',
     <header class="app-header">
         <div class="container-fluid">
             <div class="row">
-
-                <div class="col-sm-hide col-md-4">
-                    <div class="square"></div>
+                <div class="col-sm-hide col-md-4 py-4">
+                    <div class="user-img">
+                        <img src="../../../public/user-key-svgrepo-com.svg" alt="">
+                    </div>
+                    <a href="../pages/Homepage.vue">BoolBnB</a>
                 </div>
 
-                <div class="col-12 col-md-4">
-                    <div class="square"></div>
+                <div class="col-12 col-md-4 py-4"> 
+                    <div class="search-box">
+                        <button class="btn-search"><i class="fas fa-search"></i></button>
+                        <input type="text" class="input-search" placeholder="Cerca un BnB...">
+                    </div>
                 </div>
 
-                <div class="col-sm-hide col-md-4">
-                    <div class="square"></div>
+                <div class="col-sm-hide col-md-4 py-4">
+                    <a href="#">Login</a>
+                    <a href="#">Register</a>
+                    <div class="user-img">
+                       <img src="../../../public/user-key-svgrepo-com.svg" alt=""> 
+                    </div>
+                    
                 </div>
                 
             </div>
@@ -27,14 +37,101 @@ name: 'AppHeader',
 </template>
 
 <style lang="scss" scoped>
-
-    .square {
-            height: 100px;
-            width: 100%;
-            background-color: rgb(255, 102, 0);
-            border: 1px solid black;
+        .col-12{
+            padding: 0px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            text-align: center;
+            background-color: #192033;
+            .search-box{
+                width: fit-content;
+                height: fit-content;
+                position: relative;
+            }
+            .input-search{
+                height: 50px;
+                width: 50px;
+                border-style: none;
+                padding: 10px;
+                font-size: 18px;
+                outline: none;
+                border-radius: 25px;
+                transition: all .5s ease-in-out;
+                background-color: #84a59d;
+                padding-right: 40px;
+                color:#f7ede2;
+            }
+            .input-search::placeholder{
+                color:rgba(255,255,255,.5);
+                font-size: 18px;
+                font-weight: 100;
+            }
+            .btn-search{
+                width: 50px;
+                height: 50px;
+                border-style: none;
+                font-size: 20px;
+                font-weight: bold;
+                outline: none;
+                cursor: pointer;
+                border-radius: 50%;
+                position: absolute;
+                right: 0px;
+                color:#f7ede2 ;
+                background-color:transparent;
+                pointer-events: painted;  
+                &:hover{
+                    transform: scale(1.2);
+                    transition: all 0.3s ease-in-out;
+                }
+            }
+            .btn-search:focus ~ .input-search{
+                width: 300px;
+                border-radius: 0px;
+                background-color: transparent;
+                border-bottom:1px solid rgba(255,255,255,.5);
+                transition: all 500ms cubic-bezier(0, 0.110, 0.35, 2);
+            }
+            .input-search:focus{
+                width: 300px;
+                border-radius: 0px;
+                background-color: transparent;
+                border-bottom:1px solid rgba(255,255,255,.5);
+                transition: all 500ms cubic-bezier(0, 0.110, 0.35, 2);
+            }
         }
-
+        .col-md-4{
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            background-color: #192033;
+            padding: 0px 80px;
+            a{
+                color: #f7ede2;
+                text-decoration: none;
+                font-weight: bold;
+                font-size: 1.2rem;
+                margin: 0px 20px;
+                &:hover{
+                    color: #f6bd60;
+                    transform: scale(1.2);
+                    transition: all 0.3s ease-in-out;
+                }
+            }
+            .user-img{
+                height: 50px;
+                width: 50px;
+                border-radius: 50%;
+                background-color: #f7ede2;
+                img{
+                    height: 100%;
+                    width: 100%;
+                    border-radius: 50%;
+                    object-fit: cover;
+                }
+            }
+        }
         
     /* Su dispositivi mobile (fino a 767px) */
     @media (max-width: 767px) {
--- src/components/layouts/Sidebar.vue
+++ src/components/layouts/Sidebar.vue
@@ -1,102 +1,227 @@
 <script>
-    export default {
-        data() {
-            return {
-                isSidebarOpen: false, // Stato della sidebar
-            };
-        },
-        methods: {
-            toggleSidebar() {
-                this.isSidebarOpen = !this.isSidebarOpen; // Alterna lo stato della sidebar
-            },
-        },
-    };
+export default {
+	mounted() {
+		var icon = document.getElementById("icon");
+		var nav = document.getElementById("nav");
+		var blue = document.getElementById("blue");
+		var icon1 = document.getElementById("a");
+		var icon2 = document.getElementById("b");
+		var icon3 = document.getElementById("c");
+
+		// Toggle menu quando l'icona è cliccata
+		icon.addEventListener("click", function (event) {
+			icon1.classList.toggle("a");
+			icon2.classList.toggle("c");
+			icon3.classList.toggle("b");
+			nav.classList.toggle("show");
+			blue.classList.toggle("slide");
+		});
+
+		// Chiude il menu quando si clicca fuori
+		document.addEventListener("click", function (event) {
+			var isClickInsideNav = nav.contains(event.target);
+			var isClickInsideIcon = icon.contains(event.target);
+
+			// Se non si clicca sull'icona o sulla navbar, chiudi il menu
+			if (
+				!isClickInsideNav &&
+				!isClickInsideIcon &&
+				nav.classList.contains("show")
+			) {
+				icon1.classList.remove("a");
+				icon2.classList.remove("c");
+				icon3.classList.remove("b");
+				nav.classList.remove("show");
+				blue.classList.remove("slide");
+			}
+		});
+
+		// Chiude la navbar quando si cambia pagina (rotta)
+		this.$router.afterEach(() => {
+			// Rimuove le classi per chiudere la navbar
+			icon1.classList.remove("a");
+			icon2.classList.remove("c");
+			icon3.classList.remove("b");
+			nav.classList.remove("show");
+			blue.classList.remove("slide");
+		});
+	},
+};
 </script>
 
 <template>
-    <div>
-        <!-- Pulsante per attivare la sidebar -->
-        <button class="sidebar-toggle" @click="toggleSidebar">
-            {{ isSidebarOpen ? '>' : '<' }}
-        </button>
-
-        <!-- Sidebar -->
-        
-        <transition name="expand">
-            <div v-if="isSidebarOpen" class="sidebar">
-                <ul>
-                <li>Home</li>
-                <li>Destinazioni</li>
-                <li>Esperienze</li>
-                <li>Host</li>
-                <li>Aiuto</li>
-                </ul>
-            </div>
-        </transition>
-    </div>
+  	<header>
+		<div class="hamburger-icon" id="icon">
+			<div class="icon-1" id="a"></div>
+			<div class="icon-2" id="b"></div>
+			<div class="icon-3" id="c"></div>
+			<div class="clear"></div>
+		</div>
+		<nav id="nav">
+			<ul>
+				<li>
+					<router-link to="/">
+						<i class="me-3 fa-solid fa-dice-d20" v-if="$route.path === '/'"></i
+						>Home
+					</router-link>
+				</li>
+				<li>
+					<router-link to="/characters">
+						<i
+							class="me-3 fa-solid fa-dice-d20"
+							v-if="$route.path === '/characters'"></i
+						>Personaggi
+					</router-link>
+				</li>
+				<li>
+					<router-link to="/items">
+						<i
+							class="me-3 fa-solid fa-dice-d20"
+							v-if="$route.path === '/items'"></i
+						>Oggetti
+					</router-link>
+				</li>
+				<li>
+					<router-link to="/game-two">
+						<i
+							class="me-3 fa-solid fa-dice-d20"
+							v-if="$route.path === '/game-two'"></i
+						>Gioca
+					</router-link>
+				</li>
+				<li>
+					<router-link to="/who-we-are">
+						<i
+							class="me-3 fa-solid fa-dice-d20"
+							v-if="$route.path === '/who-we-are'"></i
+						>Chi siamo
+					</router-link>
+				</li>
+			</ul>
+		</nav>
+		<div class="dark-overlay" id="blue"></div>
+	</header>
 </template>
 
 <style scoped lang="scss">
-    /* Pulsante per attivare la sidebar */
-    .sidebar-toggle {
-        position: fixed;
-        top: 110px;
-        right: 0px;
-        background-color: #ff5a5f;
-        color: white;
-        padding: 10px 20px;
-        border: none;
-        cursor: pointer;
-        z-index: 10;
-    }
+    header {
+	width: 30%;
+	height: 100%;
+	position: absolute;
+	overflow: auto;
+}
 
-    .sidebar {
-        position: fixed;
-        top: 100px; 
-        right: 0;
-        height: calc(100% - 60px);
-        width: 25%;
-        background-color: #333;
-        color: white;
-        padding: 20px;
-        transition: width 0.5s ease;
-        overflow-x: hidden;
-        z-index: 4;
-    }
+.icon-1,
+.icon-2,
+.icon-3 {
+	position: absolute;
+	left: 25%;
+	top: 50%;
+	width: 32px;
+	height: 3px;
+	background-color: #d4af37; /* Oro per gli hamburger icon */
+	transition: all 400ms cubic-bezier(0.84, 0.06, 0.52, 1.8);
+}
 
-    /* Menu all'interno della sidebar */
-    .sidebar ul {
-        list-style: none;
-        padding: 0;
-        margin: 0;
-    }
+.icon-1 {
+	transform: translateY(-8px);
+	animation-delay: 100ms;
+}
 
-    .sidebar li {
-        padding: 15px 0;
-        cursor: pointer;
-        transition: background-color 0.3s;
-    }
+.icon-3 {
+	transform: translateY(8px);
+	animation-delay: 250ms;
+}
 
-    .sidebar li:hover {
-        
-    }
+.hamburger-icon {
+	position: absolute;
+	height: 60px;
+	width: 60px;
+	top: 10%;
+	left: 3%;
+	z-index: 1000;
+	cursor: pointer;
+	border-radius: 50%;
+	transition: all 0.2s ease-in-out;
+	background: rgba(48, 80, 48, 0.3);
+	&:hover {
+		transform: scale(1.2);
+		box-shadow: 0px 0px 30px rgba(0, 0, 0, 0.1);
+		background: rgba(48, 80, 48, 0.6);
+	}
+}
 
-    /* Animazioni per l'espansione della sidebar */
-        .expand-enter-active, .expand-leave-active {
-        transition: all 0.5s ease;
-    }
+.icon-1.a {
+	transform: rotate(40deg);
+}
+.icon-3.b {
+	transform: rotate(-40deg);
+}
+.icon-2.c {
+	opacity: 0;
+}
 
-    .expand-enter, .expand-leave-to {
-        opacity: 0;
-        transform: translateX(100%); 
-    }
+.clear {
+	clear: both;
+}
 
-    /* MEDIA QUERIES */
-    /* mobile */
-    @media (max-width: 767px) {
-        .sidebar {
-            width: 100vw;
-            text-align: center;
-        }
-    }
+nav {
+	background: linear-gradient(135deg, #000000, #2f4f2f);
+	position: absolute;
+	top: 0;
+	left: 0;
+	height: 100%;
+	width: 0%;
+	z-index: 10;
+	opacity: 0;
+	transition: all 600ms cubic-bezier(0.62, 0.04, 0.3, 1.56);
+	transition-delay: 100ms;
+	ul {
+		margin: 0;
+		position: absolute;
+		top: 30%;
+		left: 30%;
+		li {
+			list-style: none;
+			font-size: 24px;
+			color: #d4af37; /* Oro per i link */
+			line-height: 2.2;
+			text-transform: uppercase;
+			letter-spacing: 1.7px;
+
+			a:link,
+			a:visited {
+				text-decoration: none;
+				color: #d4af37;
+				&:hover {
+					color: #e06a1b;
+					transition: all 0.3s ease-in-out;
+				}
+			}
+		}
+	}
+}
+
+nav.show {
+	width: 75%;
+	opacity: 1;
+}
+
+.dark-overlay {
+	position: absolute;
+	top: 0;
+	left: 0;
+	background: #000000; /* Verde foresta per un'atmosfera naturale */
+	height: 100%;
+	width: 0%;
+	transition: all 500ms cubic-bezier(0.62, 0.04, 0.3, 1.8);
+	transition-delay: 50ms;
+	z-index: 5;
+	opacity: 1;
+}
+
+.dark-overlay.slide {
+	width: 50%;
+	opacity: 1;
+}
 </style>
