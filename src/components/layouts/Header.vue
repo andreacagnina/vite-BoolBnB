17a82772586abb8fcfc25d31ccd4e9e5363a3683
LorenzoPranio <lorenzopranio02@gmail.com>
2024-11-07 12:46:33 +0100

    [UPD]Added responsivity on searchbar

--- src/components/layouts/Header.vue
+++ src/components/layouts/Header.vue
@@ -9,10 +9,7 @@ name: 'AppHeader',
         <div class="container-fluid">
             <div class="row">
                 <div class="col-sm-hide col-md-4 py-4">
-                    <div class="user-img">
-                        <img src="../../../public/user-key-svgrepo-com.svg" alt="">
-                    </div>
-                    <a href="../pages/Homepage.vue">BoolBnB</a>
+                    <img class="logo" src="../../../public/logo.png" alt="Logo">
                 </div>
 
                 <div class="col-12 col-md-4 py-4"> 
@@ -87,17 +84,17 @@ name: 'AppHeader',
             }
         }
         .btn-search:focus ~ .input-search{
-            width: 300px;
+            width: 450px;
             border-radius: 0px;
             background-color: transparent;
-            border-bottom:1px solid rgba(255,255,255,.5);
+            border-bottom:1px solid #f7ede2;
             transition: all 500ms cubic-bezier(0, 0.110, 0.35, 2);
         }
         .input-search:focus{
-            width: 300px;
+            width: 450px;
             border-radius: 0px;
             background-color: transparent;
-            border-bottom:1px solid rgba(255,255,255,.5);
+            border-bottom:1px solid #f7ede2;
             transition: all 500ms cubic-bezier(0, 0.110, 0.35, 2);
         }
     }
@@ -107,6 +104,10 @@ name: 'AppHeader',
         align-items: center;
         background-color: #192033;
         padding: 0px 80px;
+        .logo{
+            width: 150px;
+            height: 100px;
+        }
         a{
             color: #f7ede2;
             text-decoration: none;
@@ -120,8 +121,8 @@ name: 'AppHeader',
             }
         }
         .user-img{
-            height: 50px;
-            width: 50px;
+            height: 60px;
+            width: 60px;
             border-radius: 50%;
             background-color: #f7ede2;
             img{
@@ -137,6 +138,43 @@ name: 'AppHeader',
     @media (max-width: 767px) {
         .col-sm-hide {
             display: none;
+        }
+        .col-12{
+            padding: 0px;
+            .btn-search:focus ~ .input-search{
+                width: 300px;
+                border-radius: 0px;
+                background-color: transparent;
+                border-bottom:1px solid #f7ede2;
+                transition: all 500ms cubic-bezier(0, 0.110, 0.35, 2);
+            }
+            .input-search:focus{
+                width: 300px;
+                border-radius: 0px;
+                background-color: transparent;
+                border-bottom:1px solid #f7ede2;
+                transition: all 500ms cubic-bezier(0, 0.110, 0.35, 2);
+            }
+        }
+    }
+    /* Su dispositivi tablet (da 768px a 991px) */
+    @media (min-width: 768px) and (max-width: 991px) {
+        .col-md-4 {
+            padding: 0px 20px;
+            .btn-search:focus ~ .input-search{
+                width: 300px;
+                border-radius: 0px;
+                background-color: transparent;
+                border-bottom:1px solid #f7ede2;
+                transition: all 500ms cubic-bezier(0, 0.110, 0.35, 2);
+            }
+            .input-search:focus{
+                width: 300px;
+                border-radius: 0px;
+                background-color: transparent;
+                border-bottom:1px solid #f7ede2;
+                transition: all 500ms cubic-bezier(0, 0.110, 0.35, 2);
+            }
         }
     }
 </style>
