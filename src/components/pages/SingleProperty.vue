c9533b2cdecfae7806117e91bf7daec31bd16b1b
diego ferrari <167826265+diegoferrari129@users.noreply.github.com>
2024-11-12 23:11:11 +0100

    [UPD] update carousel

--- src/components/pages/SingleProperty.vue
+++ src/components/pages/SingleProperty.vue
@@ -6,23 +6,35 @@ export default {
     data() {
         return {
             store,
-            isModalVisible: false,  // Stato per mostrare/nascondere la modale
-            isLoading: true,        // Stato per gestire il caricamento
+            isModalVisible: false,
+            isLoading: true,
             form: {
                 firstName: '',
                 lastName: '',
                 email: '',
                 message: '',
             },
-            isMessageSent: false,  // Stato per mostrare il messaggio di successo
-            showMessageClass: '',  // Classe per gestire l'animazione del messaggio
+            isMessageSent: false,
+            showMessageClass: '',
+            currentImageIndex: 0, // Indice per l'immagine attiva nel carosello
         };
     },
     created() {
         this.getProperty();
     },
+    computed: {
+        currentImage() {
+            return this.images[this.currentImageIndex];
+        },
+        visibleSmallImages() {
+            // Ottieni un array con tutte le immagini tranne quella attualmente selezionata
+            return this.images.filter((_, index) => index !== this.currentImageIndex).slice(0, 4);
+        },
+        images() {
+            return [{ path: store.property.cover_image }, ...store.property.images];
+        },
+    },
     methods: {
-        // Funzione per recuperare la proprietà
         getProperty() {
             const slug = this.$route.params.slug;
             this.isLoading = true;
@@ -30,7 +42,7 @@ export default {
             axios.get(`${store.baseUrl}/property/${slug}`)
                 .then(response => {
                     if (response.data && response.data.results) {
-                        store.property = response.data.results;  // Salva nel store
+                        store.property = response.data.results;
                     } else {
                         console.error("Dati non validi ricevuti:", response);
                     }
@@ -42,8 +54,6 @@ export default {
                     this.isLoading = false;
                 });
         },
-
-        // Funzione per inviare il form
         submitForm() {
             if (this.isValidForm()) {
                 this.isLoading = true;
@@ -53,92 +63,96 @@ export default {
                     last_name: this.form.lastName,
                     email: this.form.email,
                     message: this.form.message,
-                    property_id: store.property.id,  // ID della proprietà corrente
-
+                    property_id: store.property.id,
                 })
-                    .then(response => {
-                        this.isMessageSent = true;
-                        this.showMessageClass = 'message-shown';
+                .then(response => {
+                    this.isMessageSent = true;
+                    this.showMessageClass = 'message-shown';
 
-                        // Reset dei campi del form
-                        this.form.firstName = '';
-                        this.form.lastName = '';
-                        this.form.email = '';
-                        this.form.message = '';
+                    this.form.firstName = '';
+                    this.form.lastName = '';
+                    this.form.email = '';
+                    this.form.message = '';
 
-                        // Nascondi il messaggio di successo dopo 3 secondi
+                    setTimeout(() => {
+                        this.showMessageClass = 'message-hidden';
                         setTimeout(() => {
-                            this.showMessageClass = 'message-hidden';
-                            setTimeout(() => {
-                                this.isMessageSent = false;
-                                this.showMessageClass = '';
-                            }, 500);
-                        }, 3000);
-                    })
-                    .catch(error => {
-                        console.error("Errore nell'invio del messaggio:", error);
-                        alert("C'è stato un errore nell'invio del messaggio. Riprova.");
-                    })
-                    .finally(() => {
-                        this.isLoading = false;
-                    });
+                            this.isMessageSent = false;
+                            this.showMessageClass = '';
+                        }, 500);
+                    }, 3000);
+                })
+                .catch(error => {
+                    console.error("Errore nell'invio del messaggio:", error);
+                    alert("C'è stato un errore nell'invio del messaggio. Riprova.");
+                })
+                .finally(() => {
+                    this.isLoading = false;
+                });
             }
         },
-
-        // Funzione per validare il form
         isValidForm() {
-            if ( !this.form.email || !this.form.message) {
+            if (!this.form.email || !this.form.message) {
                 alert("Tutti i campi sono obbligatori!");
                 return false;
             }
             return this.validateEmail(this.form.email);
         },
-
-        // Funzione di validazione email
         validateEmail(email) {
             const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
             return re.test(email);
         },
-
-        // Funzione per mostrare/nascondere la modale
         toggleModal() {
             this.isModalVisible = !this.isModalVisible;
+        },
+        setCurrentImage(index) {
+            this.currentImageIndex = index + 1;  // Compensa l'offset della cover_image
+        },
+        prevImage() {
+            this.currentImageIndex = (this.currentImageIndex === 0)
+                ? this.images.length - 1
+                : this.currentImageIndex - 1;
+        },
+        nextImage() {
+            this.currentImageIndex = (this.currentImageIndex === this.images.length - 1)
+                ? 0
+                : this.currentImageIndex + 1;
         }
+    
     }
 };
 </script>
 
+
 <template>
     <div class="container">
         <div class="row">
-            <!-- Immagine principale (cover image) -->
-            <div class="col-md-8">
-                <img :src="store.property.cover_image" alt="Main Property Image" class="img-fluid main-image ">
+            <!-- Immagine principale (immagine attiva selezionata) -->
+            <div class="col-md-7">
+                <img :src="currentImage.path" alt="Main Property Image" class=" img-fluid main-image rounded-4">
             </div>
-    
+
             <!-- Immagini aggiuntive (layout 2x2) -->
-            <div class="col-md-4">
+            <div class="col-md-5">
                 <div class="small-images d-flex flex-column h-100">
                     <div class="d-flex flex-grow-1">
                         <img 
-                            v-for="(image, index) in store.property.images.slice(0, 2)" 
+                            v-for="(image, index) in visibleSmallImages" 
                             :key="image.id" 
                             :src="image.path" 
                             alt="Additional Property Image" 
-                            class="img-fluid w-50 small-image m-1"
-                        />
-                    </div>
-                    <div class="d-flex flex-grow-1">
-                        <img 
-                            v-for="(image, index) in store.property.images.slice(2, 4)" 
-                            :key="image.id" 
-                            :src="image.path" 
-                            alt="Additional Property Image" 
-                            class="img-fluid w-50 small-image m-1"
+                            class="img-fluid w-50 small-image m-1 rounded-4"
+                            @click="setCurrentImage(index)"
                         />
                     </div>
                 </div>
             </div>
+        </div>
+
+        <!-- Bottoni per scorrere le immagini -->
+        <div class="d-flex justify-content-center mt-2">
+            <button @click="prevImage" class="btn btn-secondary mx-1">Previous</button>
+            <button @click="nextImage" class="btn btn-secondary mx-1">Next</button>
         </div>
     </div>
     
@@ -217,6 +231,43 @@ export default {
 
 
 <style lang="scss" scoped>
+
+.main-image {
+    height:550px;
+    width: 100%;
+    object-fit: cover;
+}
+
+.small-images {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+    position: relative;
+}
+
+.small-image {
+    object-fit: cover;
+}
+
+.prev, .next {
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+    z-index: 10;
+}
+
+.prev {
+    left: -10px;
+}
+
+.next {
+    right: -10px;
+}
+
+.small-image.active {
+    border: 2px solid #49919d;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+}
 /* Messaggio di successo */
 .success-message {
     background-color: #49919d;
@@ -242,20 +293,6 @@ export default {
     margin-right: 10px;
 }
 
-.main-image {
-    height: 100%;
-    object-fit: cover;
-}
-
-.small-images {
-    display: flex;
-    flex-direction: column;
-    height: 100%;
-}
-
-.small-image {
-    object-fit: cover;
-}
 
 /* Animazione di entrata */
 @keyframes slideIn {
