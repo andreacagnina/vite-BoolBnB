596737bb1ed0e897821e8eac5b78b03f27e33ccb
diego ferrari <167826265+diegoferrari129@users.noreply.github.com>
2024-11-17 21:04:00 +0100

    [UPD] added style and content to footer

--- src/components/layouts/Footer.vue
+++ src/components/layouts/Footer.vue
@@ -1,8 +1,12 @@
 <script>
+
+import footerMenu from '../../data/footerMenu.json';
+
 export default {
 
     data() {
         return {
+            footerMenu,
             isFooterOpen: false, // Stato per il menu aggiuntivo
         };
     },
@@ -21,27 +25,32 @@ export default {
         <div class="container">
             <div class="footer-content" @click="toggleFooter">
 
-                <h3 class="footer-toggle-text">
-                    <strong>{{ isFooterOpen ? 'View less!' : 'View more!' }}</strong>
+                <h3 class="footer-toggle-text pb-4 pt-3">
+                    <strong>
+                        <i class="fa-solid fa-angles-down fa-lg" 
+                        :class="isFooterOpen ? 'fa-flip-horizontal' : 'fa-flip-vertical'"></i>
+                        <span class="ms-2">{{ isFooterOpen ? 'Hide' : 'Expand' }}</span>
+                    </strong>
                 </h3>
+                
+                <div class="social-icon d-flex align-items-center justify-content-evenly mb-0 pb-0">
 
-                <div class="social-icon d-flex justify-content-between align-items-center">
+                    <p>&copy; 2024 BoolBnB. All rights reserved.</p>
 
                     <ul class="d-flex">
-                        <li>
-                            <a href="">&copy; 2024 BoolBnB. All rights reserved.</a>
-                        </li>
                         <li>
                             <a href="">Terms</a>
                         </li>
                         <li>
                             <a href="">Sitemap</a>
                         </li>
+                        <li>
+                            <a href="">Privacy</a>
+                        </li>
                     </ul>
 
-                    <div class="d-flex align-items-end">
+                    <div class="d-flex align-items-end justify-content-end ms-5">
                         
-                        <p class="follow-us pe-2"><strong>Follow us on</strong></p>
                         <p><i class="fab fa-facebook-f"></i> <i class="fab fa-twitter"></i> <i class="fab fa-instagram"></i> <i class="fa-brands fa-linkedin"></i></p>
 
                     </div>
@@ -54,14 +63,20 @@ export default {
             <transition name="expand">
 
                 <div v-if="isFooterOpen" class="additional-footer-menu">
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-12">  
-                                <div class="text-center my-3">
-                                    <router-link class="footer-link mx-2" >Link placeholder 1</router-link>
-                                    <router-link class="footer-link mx-2" >Link placeholder 2</router-link>
-                                    <router-link class="footer-link mx-2" >Link placeholder 3</router-link>
-                                    <router-link class="footer-link mx-2" >Link placeholder 4</router-link>
+
+                    <div class="row">
+                        <div class="col-12">  
+                            <div class="d-flex justify-content-center">
+                                <div v-for="section in footerMenu" :key="section.section" class="">
+                                    
+                                    <ul class="text-start">
+                                        <li>
+                                            <h4 class="fw-bold">{{ section.section }}</h4>
+                                        </li>
+                                        <li v-for="link in section.links" :key="link.url">
+                                            <a :href="link.url">{{ link.name }}</a>
+                                        </li>
+                                    </ul>
                                 </div>
                             </div>
                         </div>
@@ -99,16 +114,15 @@ export default {
 </template>
 
 <style scoped lang="scss">
-
+    @use "../../styles/partials/partials.scss" as *;
 .app-footer {
 
     position: relative;
     z-index: 1;
     margin-top: auto;
     background-color: #49919d;
-    padding: 20px;
+    padding-bottom: 20px;
     text-align: center;
-    cursor: pointer;
     font-size: 1rem;
     color: #f7ede2;
 
@@ -116,14 +130,14 @@ export default {
 
 /* Pulsante per aprire il Menu aggiuntivo */
 .footer-toggle-text {
-    margin-top: 10px;
     font-size: 1rem;
     text-decoration: none;
+    background: linear-gradient(45deg, #f6bd60, #ce6a6c);
+    background-clip: text;
+    color: transparent;
 
     &:hover {
-        background: linear-gradient(45deg, #f6bd60, #ce6a6c);
-        background-clip: text;
-        color: transparent;
+        cursor: pointer;
         transform: scale(1.15);
         transition: all 0.3s ease-in-out;
     }
@@ -137,18 +151,25 @@ export default {
     bottom: 100%;
     left: 0;
     width: 100%;
-    height: 150px;
-    padding: 10px 0;
-    z-index: 5;
-    display: flex;
-    justify-content: center;
+    z-index: 1;
+    overflow-x: hidden;
 
         ul {
             list-style: none;
-            display: flex;
+            padding-top: 20px;
+            color: black;
 
             li {
-                margin: auto 20px;
+                padding: 5px 40px;
+
+                a {
+                    color: black;
+                    text-decoration: none;
+
+                    &:hover {
+                        text-decoration: underline;
+                    }
+                }
             }
         }
 }
@@ -168,6 +189,24 @@ export default {
             color: transparent;
             transform: scale(1.2);
             transition: all 0.3s ease-in-out;
+        }
+    }
+
+    ul  {
+        li {
+            padding-right: 25px;
+
+            li-cust {
+                text-decoration: none; }
+
+            a {
+                color: #f7ede2;
+                text-decoration: none;
+
+                &:hover {
+                    text-decoration: underline;
+                }
+            }
         }
     }
 }
